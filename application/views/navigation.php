d8cc42764214f65b325902be4d1ac1c031cebdfa
divyeshbalar <divyeshkumar_balar@outlook.com>
2018-08-09 05:05:46 -0400

    restaurant name added

--- application/views/navigation.php
+++ application/views/navigation.php
@@ -5,7 +5,7 @@
 
 			<div class="row" >
 				<div class="col-sm-4 col-xs-12">
-					<div id="gtco-logo"><a href="<?php echo base_url() ?>index.php">Pizza <em style="font-size: small;">. Restaurant</em></a></div>
+					<div id="gtco-logo"><a href="<?php echo base_url() ?>index.php">IEat <em style="font-size: small;">. Restaurant</em></a></div>
 				</div>
 				<div class="col-xs-8 text-right menu-1">
 					<ul>
